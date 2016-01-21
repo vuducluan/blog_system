e2c2d7738cf283360900fc208be8686758cd9ebe
Vu Duc Luan <vu.duc.luan@framgia.com>
2016-01-21 16:58:47 +0700

    Finish user edit, update, index, and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(name:  "Vu Duc Luan",
+             email: "vuvanlyth@gmail.com",
+             password:              "coltech",
+             password_confirmation: "coltech",
+             admin: true)
+
+
+20.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
